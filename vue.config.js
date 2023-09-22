838b64836c5c5ceb6b772b178af8516068b40758
fukugit <fukuma1234@gmail.com>
2023-09-22 11:47:11 +0900

    Add comment for publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // This setting is for publication of Github pages.
   publicPath: process.env.NODE_ENV === "production" ? "/todo-list-vue3/" : "/",
 })
